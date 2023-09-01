0e33e7cb31c9de6f44caaefd77a1b32ccd0b02ab
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-09-01 16:42:33 -0300

    could not match bytecode

--- Echidna.sol
+++ Echidna.sol
@@ -4,12 +4,21 @@ pragma solidity 0.8.17;
 
 import "@crytic/properties/contracts/util/PropertiesHelper.sol";
 import "./Deployer.sol";
+import "./Governor.sol";
 
 contract EchidnaTester is PropertiesAsserts {
     Deployer internal deployer;
 
     constructor() {
         deployer = new Deployer();
+
+        bytes memory creationCode = type(Governor).creationCode;
+        bytes memory args = abi.encode(address(this));
+
+        address authority = deployer.deploy(
+            deployer.AUTHORITY(),
+            abi.encodePacked(creationCode, args)
+        );
     }
 
     function test1p1eq2() public {
